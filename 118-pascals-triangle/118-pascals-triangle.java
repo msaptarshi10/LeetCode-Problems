9ba3c9ee8c417cd753900f4720e465bcca8b430b
msaptarshi10 <109888153+msaptarshi10@users.noreply.github.com>
2022-07-24 12:54:15 +0530

    Time: 1 ms (72.34%), Space: 41.6 MB (71.55%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+         List<List<Integer>>subset = new ArrayList<>();
+        int[][] arr = new int[numRows][numRows];
+        if(numRows <= 0)
+            return subset;
+        for(int i = 0;i < numRows;i++){
+            List<Integer>list = new ArrayList<>();
+            for(int j = 0;j < i + 1;j++){
+                if(j == 0||j == i)
+                    list.add(1);
+                else
+                    list.add(subset.get(i - 1).get(j - 1) + subset.get(i - 1).get(j));
+            }
+            subset.add(list);
+        }
+        return subset;
+    }
+    
+}
